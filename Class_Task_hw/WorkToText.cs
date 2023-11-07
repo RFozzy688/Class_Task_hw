00cc016f371ec585ac9c557ea78fe654cb22daf3
RFozzy688 <pokerfozzy@gmail.com>
2023-11-07 18:26:16 +0200

    class WorkToText

---
+++ Class_Task_hw/WorkToText.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Task_hw
+{
+    internal class WorkToText
+    {
+        string _text;
+
+        public WorkToText()
+        {
+            using (FileStream fs = new FileStream("test.txt", FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    _text = sr.ReadToEnd();
+                }
+            }
+        }
+        public int NumberOfSentences()
+        {
+            string[] separator = { ". ", "! ", "? " };
+            string[] sentences = null;
+
+            sentences = _text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            return sentences.Length;
+        }
+    }
+}
