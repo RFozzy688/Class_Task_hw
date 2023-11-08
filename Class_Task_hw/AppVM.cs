b2a3f73d436568ae9b529b53f9ba31a8085cba47
RFozzy688 <pokerfozzy@gmail.com>
2023-11-08 06:49:48 +0200

    вывод результатов работы задач на экран

--- Class_Task_hw/AppVM.cs
+++ Class_Task_hw/AppVM.cs
@@ -13,9 +13,8 @@ namespace Class_Task_hw
     public delegate int TextDelegate();
     struct TaskInfo
     {
-        public int _number;
         public Task<int> _task;
-        public TextBlock _textBlocks;
+        public string _text;
     }
     internal class AppVM
     {
@@ -24,8 +23,9 @@ namespace Class_Task_hw
         List<TextDelegate> _methods;
         List<TaskInfo> _tasks;
         List<TextBlock> _textBlocks;
+        List<CheckBox> _checkBoxes;
         Commands _getMethod;
-        Commands _getAction;
+        Commands _getAction;int c = 0;
 
         public AppVM(MainWindow view)
         {
@@ -35,6 +35,7 @@ namespace Class_Task_hw
             _methods = new List<TextDelegate>();
             _tasks = new List<TaskInfo>();
             _textBlocks = new List<TextBlock>();
+            _checkBoxes = new List<CheckBox>();
 
             _textBlocks.Add(_view.Text1);
             _textBlocks.Add(_view.Text2);
@@ -42,33 +43,48 @@ namespace Class_Task_hw
             _textBlocks.Add(_view.Text4);
             _textBlocks.Add(_view.Text5);
 
+            _checkBoxes.Add(_view.Check1);
+            _checkBoxes.Add(_view.Check2);
+            _checkBoxes.Add(_view.Check3);
+            _checkBoxes.Add(_view.Check4);
+            _checkBoxes.Add(_view.Check5);
+
             _getMethod = new Commands(ChoiceMethod);
             _getAction = new Commands(ActionMethod);
 
             _methods.Add(_workToText.NumberOfSentences);
-            _methods.Add(_workToText.NumberOfSentences);
-            _methods.Add(_workToText.NumberOfSentences);
-            _methods.Add(_workToText.NumberOfSentences);
-            _methods.Add(_workToText.NumberOfSentences);
+            _methods.Add(_workToText.NumbreOfSimbols);
+            _methods.Add(_workToText.NumbreOfWords);
+            _methods.Add(_workToText.NumberOfInterrogativeSentences);
+            _methods.Add(_workToText.NumberOfExclamationSentences);
         }
         public Commands GetMethod { get { return _getMethod; } }
         public Commands GetAction { get { return _getAction; } }
         private void ChoiceMethod(object param)
         {
-            int index = _tasks.FindIndex(o => o._number == Convert.ToInt32(param));
+            int index = Convert.ToInt32(param);
 
-            if (index == -1)
+            if (_view.SaveToFile.IsChecked == true )
             {
                 TaskInfo taskInfo = new TaskInfo();
-                taskInfo._task = new Task<int>(() => _methods[Convert.ToInt32(param)]());
-                taskInfo._number = Convert.ToInt32(param);
-                taskInfo._textBlocks = _textBlocks[Convert.ToInt32(param)];
+                taskInfo._task = new Task<int>(() => _methods[index]());
+                taskInfo._text = _checkBoxes[index].Content.ToString();
 
                 _tasks.Add(taskInfo);
             }
-            else
+            else if(_view.OutputOnScreen.IsChecked == true)
             {
-                _tasks.RemoveAt(index);
+                if (_textBlocks[index].Text == "")
+                {
+                    Task<int> task = new Task<int>(() => _methods[index]());
+                    task.Start();
+
+                    _textBlocks[index].Text = task.Result.ToString();
+                }
+                else
+                {
+                    _textBlocks[index].Text = "";
+                }
             }
         }
         private void ActionMethod(object param)
@@ -76,7 +92,7 @@ namespace Class_Task_hw
             foreach (var task in _tasks)
             {
                 task._task.Start();
-                task._textBlocks.Text = task._task.Result.ToString();
+                //task._textBlocks.Text = task._task.Result.ToString();
             }
         }
     }
